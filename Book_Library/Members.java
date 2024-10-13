de05c7001551333d8b2072c42512466e3ec1e047
mujju-0 <150520430+mujju-0@users.noreply.github.com>
2024-10-13 15:06:38 +0530

    Update Members.java
    improved books features

--- Book_Library/Members.java
+++ Book_Library/Members.java
@@ -3,7 +3,8 @@ package Book_Library;
 public class Members {
 	private String name;
 	private int id;
-	private String borrowedBooks;
+	private String borrowedBooks[] = new String[100]; //It indicates how many books can borrow a user
+	int count = 0;
 	
 	public String getName() {
 		return name;
@@ -17,10 +18,14 @@ public class Members {
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getBorrowedBooks() {
-		return borrowedBooks;
+	public void getBorrowedBooks() {
+
+		for(int i = 0; i<count; i++) {
+			System.out.print(" Title: "+borrowedBooks[i]+" ");
+			}
 	}
 	public void setBorrowedBooks(String borrowedBooks) {
-		this.borrowedBooks = borrowedBooks;
+		this.borrowedBooks[count] = borrowedBooks;
+		count++;
 	}
 }
