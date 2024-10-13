c06d9fbdf6bbc5070f0573956e112719d1a2e9c8
mujju-0 <150520430+mujju-0@users.noreply.github.com>
2024-10-13 15:00:27 +0530

    Update Library.java
    Improved more features in Book Library

--- Book_Library/Library.java
+++ Book_Library/Library.java
@@ -4,91 +4,174 @@ import java.util.Scanner;
 
 public class Library {
 	
-	Scanner sc = new Scanner(System.in);
-	private BookData[] books = new BookData[10]; // Holds books
-	private Members[] members = new Members[10]; // Holds members
-	private int bookCount = 7; // Current number of books
-	private int memberCount = 0; // Current number of members
+
 	
-	// Initialize some books in the library
-	void initializeBooks() {
-		String titles[] = {"a", "b", "c", "d", "e", "f", "g"};
-		String authors[] = {"sohel", "sohel.j", "javed", "adnna", "pasha", "chicha", "gunda"};
-		String isbns[] = {"12-21", "32-1", "123", "098", "343", "423", "780"};
-		String statuses[] = {"available", "unavailable", "available", "available", "unavailable", "available", "available"};
-		
-		for (int i = 0; i < bookCount; i++) {
+	static Scanner sc = new Scanner(System.in);
+	private BookData[] books = new BookData[10];
+	static private Members[] members = new Members[10];
+	static int Bookcount = 6;
+	static int Membercount = 0;
+//	Assienging our books
+	void CurrentBooks() 
+	{
+	String titles[] = {"a", "b", "c", "d", "e", "f", "g"};
+	String authors[] = {"sohel", "sohel.j", "javed", "adnna", "pasha", "chicha", "gunda"};
+	String isbns[] = {"12-21", "32-1", "123", "098", "343", "423", "780"};
+	String statuses[] = {"available", "available", "available", "available", "available", "available", "available"};
+	
+	for(int i = 0; i<=Bookcount;i++) 
+		{
 			books[i] = new BookData();
 			books[i].setTitle(titles[i]);
 			books[i].setAuthor(authors[i]);
 			books[i].setIsbn(isbns[i]);
 			books[i].setStatus(statuses[i]);
+
 		}
 	}
 	
-	// Add a new book to the library
-	void addBook() {
-		if (bookCount >= books.length) {
-			System.out.println("Library is full! Cannot add more books.");
+//	it store available books & we access it by using getter methods 
+	void displayCurrrentBooks() {
+		System.out.println("These are availabe books");
+		for(int i = 0; i<=Bookcount;i++) 
+		{
+//			books[i] = new BookData(); it return null
+			System.out.println("Title: " +books[i].getTitle()+
+						" Author: "+books[i].getAuthor()+
+						" Isbn: "+books[i].getIsbn()+
+						" Status: "+books[i].getStatus());	
+			
+		}
+	}
+	
+//	Member/user can add it's books
+	void addBooks() 
+	{
+				if(Bookcount>books.length-1) {
+			System.out.println("No more space");
 			return;
 		}
-		
-		BookData newBook = new BookData();
-		System.out.println("Enter book title: ");
-		newBook.setTitle(sc.nextLine());
-		System.out.println("Enter book author: ");
-		newBook.setAuthor(sc.nextLine());
-		System.out.println("Enter book ISBN: ");
-		newBook.setIsbn(sc.nextLine());
-		newBook.setStatus("available");
-		
-		books[bookCount++] = newBook;
-		System.out.println("Book added successfully!");
+		System.out.println("Enter your name:");
+		String member = sc.nextLine();
+		for(int j = 0; j<=Membercount; j++) 
+		{
+			if(member.equalsIgnoreCase(members[j].getName())) 
+			{
+		Bookcount++;
+		books[Bookcount] = new BookData();
+		System.out.println("Enter title: ");
+		books[Bookcount].setTitle(sc.nextLine());
+		System.out.println("Enter Author name: ");
+		books[Bookcount].setAuthor(sc.nextLine());		
+		System.out.println("Enter ISBN no.: ");
+		books[Bookcount].setIsbn(sc.nextLine());		
+		System.out.println("Enter Status : ");
+		books[Bookcount].setStatus(sc.nextLine());
+
+		return;
+			}
+			}
+		System.out.println("member not found");
+		return;
 	}
 	
-	// Display all available books
-	void displayAvailableBooks() {
-		System.out.println("Available Books:");
-		for (int i = 0; i < bookCount; i++) {
-			if (books[i].getStatus().equalsIgnoreCase("available")) {
-				System.out.println("Title: " + books[i].getTitle() + ", Author: " + books[i].getAuthor());
+//	adding member in our library
+	void addMember() 
+	{
+		
+		
+		members[Membercount] = new Members();
+		System.out.println("Enter your name");
+		members[Membercount].setName(sc.nextLine());
+		System.out.println("Enter your Id");
+		members[Membercount].setId(sc.nextInt());
+		sc.nextLine();
+		System.out.println("Only with this information you can Enter our Library\n");
+		System.out.println("Name: "+members[Membercount].getName()+" Id: "+members[Membercount].getId()+"\n\n");
+		++Membercount;
+		
+	}
+//	Only user can get books(here user can borrow(get) books)
+	void GetBook() 
+	{
+		String title, member;
+		
+		System.out.println("Enter your name first");
+		member = sc.nextLine();
+		
+		for(int j = 0; j<Membercount; j++) 
+		{
+			if(member.equalsIgnoreCase(members[j].getName())) 
+			{
+				System.out.println("you can borrow a Book\n");
+				System.out.println("Enter Book title");
+				title = sc.nextLine();
+				for(int i = 0; i<=Bookcount; i++) 
+				{
+					if(title.equalsIgnoreCase(books[i].getTitle()) && books[i].getStatus().equalsIgnoreCase("available")) 
+					{
+						books[i].setStatus("unavailable");
+						members[j].setBorrowedBooks(books[i].getTitle());
+						System.out.println("title: "+books[i].getTitle()+" User name: "+members[j].getName());
+//						System.out.println("title: "+members[j].getBorrowedBooks()+" User name: "+members[j].getName());
+//						Membercount++;
+						return;
+					}
+					else {
+						System.out.println("false");
+					}
+
+				}
+				System.out.println("book is not available");
+				return;
+		
+			
 			}
+
+		}
+		System.out.println("user is not found");
+		return;
+	}
+	
+//	here we are showing how many books borrowed & who borrowed
+	void BorrowedBooks() 
+	{
+		System.out.println("Below are borrowed books");
+		for(int i = 0; i<Membercount; i++) 
+		{
+			System.out.print(" User name: "+members[i].getName());
+			members[i].getBorrowedBooks();
+			System.out.println();
 		}
 	}
-	
-	// Display all borrowed books
-	void displayBorrowedBooks() {
-		System.out.println("Borrowed Books:");
-		for (int i = 0; i < bookCount; i++) {
-			if (books[i].getStatus().equalsIgnoreCase("unavailable")) {
-				System.out.println("Title: " + books[i].getTitle() + ", Author: " + books[i].getAuthor());
-			}
-		}
-	}
-	
-	// Allow a member to borrow a book if available
-	void borrowBook(String title, Members member) {
-		for (int i = 0; i < bookCount; i++) {
-			if (books[i].getTitle().equalsIgnoreCase(title) && books[i].getStatus().equalsIgnoreCase("available")) {
-				books[i].setStatus("unavailable");
-				member.setBorrowedBooks(books[i].getTitle());
-				System.out.println(member.getName() + " has borrowed " + books[i].getTitle());
+//	user should return borrowed books 
+	void ReturnBook() 
+	{
+		System.out.println("Which book you wanna return:");
+		String title, member;
+		System.out.println("Enter your name first");
+		member = sc.nextLine();
+		
+		for(int j = 0; j<=Membercount; j++) 
+		{
+			if(member.equalsIgnoreCase(members[j].getName())) 
+			{
+				System.out.println("Enter book name");
+				
+				title = sc.nextLine();
+				for(int i = 0; i<books.length;i++) {
+					if(books[i].getTitle().equalsIgnoreCase(title) && books[i].getStatus().equalsIgnoreCase("unavailable")) {
+						members[j].setBorrowedBooks(null);
+						books[i].setStatus("available");
+						System.out.println("Thanks for returning book");
+						return;
+					}
+				}
+				System.out.println("book is not found");
 				return;
 			}
 		}
-		System.out.println("Book is unavailable or does not exist.");
+		System.out.println("member is not registerd");
 	}
 	
-	// Allow a member to return a book
-	void returnBook(String title, Members member) {
-		for (int i = 0; i < bookCount; i++) {
-			if (books[i].getTitle().equalsIgnoreCase(title) && books[i].getStatus().equalsIgnoreCase("unavailable")) {
-				books[i].setStatus("available");
-				member.setBorrowedBooks(null);
-				System.out.println(member.getName() + " has returned " + books[i].getTitle());
-				return;
-			}
-		}
-		System.out.println("Book was not borrowed.");
-	}
 }
